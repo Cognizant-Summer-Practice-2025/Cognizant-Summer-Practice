33425a85a747de735e138e54c7f60656efcfc3e2
Theo <teosandu88@gmail.com>
2025-07-14 18:18:27 +0300

    user API started

---
+++ backend/backend-user/Repositories/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace backend_user.Repositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetUserById(string id);
+        Task<User> GetUserByEmail(string email);
+        Task<List<User>> GetAllUsers();
+        Task<User> CreateUser(User user);
+    }
+}
---
+++ backend/backend-user/Repositories/UserRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace backend_user.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUserById(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> GetUserByEmail(string email)
+        {
+            return await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task<List<User>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> CreateUser(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+        
+        
+        
+    }
+}
