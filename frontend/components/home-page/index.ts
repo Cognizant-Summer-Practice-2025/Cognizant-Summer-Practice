09bc8f6d83ea28e50e4f3f2710d7de382ec20331
Theo <teosandu88@gmail.com>
2025-07-24 16:14:18 +0300

    worked on the skills filter section

---
+++ frontend/components/home-page/index.ts
@@ -0,0 +1,8 @@
+export { default as FilterSidebar } from './filter-sidebar';
+export { default as FilterGroup } from './filter-group';
+export { default as SelectedFilters } from './selected-filters';
+export { default as SkillsFilter } from './skills-filter';
+export { default as MobileFilter } from './mobile-filter';
+export { default as ModernSearch } from './modern-search';
+export * from './filter-types';
+export * from './filter-utils'; 
